0818e7ced5d4a1f2fa3414d7695ad4febbdcdc2c
holabayor <liasu.olabayo@gmail.com>
2022-10-04 16:29:09 +0100

    fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Square Module """
 
 
-class square():
+class Square():
     """
     Square Class
     """
@@ -28,7 +28,8 @@ class square():
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    """ Create instance """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
